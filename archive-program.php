4c8004765603784ab197cff7fc11f5f128bbd041
Hamza <6793siddique@gmail.com>
2024-10-19 15:57:04 +0500

    feat: add program

--- archive-program.php
+++ archive-program.php
@@ -1,32 +1,29 @@
-<?php get_header(); ?>
+<?php
 
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('/images/ocean.jpg') ?>)">
-    </div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"> All Programs
-        </h1>
-        <div class="page-banner__intro">
-            <p><?php the_archive_description(); ?></p>
-        </div>
-    </div>
-</div>
+get_header();
+pageBanner(array(
+  'title' => 'All Programs',
+  'subtitle' => 'There is something for everyone. Have a look around.'
+));
+ ?>
 
 <div class="container container--narrow page-section">
-    <ul class="link-list min-list">
-        <?php
 
+<ul class="link-list min-list">
 
-while(have_posts()) {
+<?php
+  while(have_posts()) {
     the_post(); ?>
-
-        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-
-        <?php
-}
-echo paginate_links();
+    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+  <?php }
+  echo paginate_links();
 ?>
-    </ul>
+</ul>
+
+
+
 </div>
 
-<?php get_footer(); ?>
+<?php get_footer();
+
+?>
--- single-program.php
+++ single-program.php
@@ -1,128 +1,108 @@
 <?php
+  
+  get_header();
 
-get_header();
+  while(have_posts()) {
+    the_post();
+    pageBanner();
+     ?>
 
-while(have_posts()) {
-    the_post(); ?>
-<div class="page-banner">
-    <div class="page-banner__bg-image"
-        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-            <p>DONT FORGET TO REPLACE ME LATER (single-program)</p>
-        </div>
-    </div>
-</div>
+    <div class="container container--narrow page-section">
+          <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+      </div>
 
-<div class="container container--narrow page-section">
-    <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?= get_post_type_archive_link('program'); ?>"><i class="fa fa-home"
-                    aria-hidden="true"></i> All Programs</a> <span class="metabox__main">
-                <?php the_title(); ?>
-            </span></p>
-    </div>
+      <div class="generic-content"><?php the_field('main_body_content'); ?></div>
 
-    <div class="generic-content"><?php the_content(); ?></div>
+      <?php 
+        $relatedProfessors = new WP_Query(array(
+          'posts_per_page' => -1,
+          'post_type' => 'professor',
+          'orderby' => 'title',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'related_programs',
+              'compare' => 'LIKE',
+              'value' => '"' . get_the_ID() . '"'
+            )
+          )
+        ));
 
-
-    <?php
-            
-
-    $related_professors = new WP_Query([
-        'posts_per_page' => -1,
-        'post_type' => 'professor',
-      
-        // 'orderby' => 'meta_value_num',
-        'order' => 'ASC',
-        'meta_query' => [
-            [
-                'key' => 'related_programs',
-                'compare' => 'LIKE',
-                'value' => '"' . get_the_ID() . '"',
-            ]
-        ]
-    ]);
-
-    if($related_professors->have_posts()) {
-        echo '<hr class="section-break">';
-
+        if ($relatedProfessors->have_posts()) {
+          echo '<hr class="section-break">';
         echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
-        while($related_professors->have_posts()) {
-            $related_professors->the_post();
-            ?>
 
-    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-    <?php
+        echo '<ul class="professor-cards">';
+        while($relatedProfessors->have_posts()) {
+          $relatedProfessors->the_post(); ?>
+          <li class="professor-card__list-item">
+            <a class="professor-card" href="<?php the_permalink(); ?>">
+              <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape') ?>">
+              <span class="professor-card__name"><?php the_title(); ?></span>
+            </a>
+          </li>
+        <?php }
+        echo '</ul>';
         }
 
-    }
+        wp_reset_postdata();
 
-    ?>
+        $today = date('Ymd');
+        $homepageEvents = new WP_Query(array(
+          'posts_per_page' => 2,
+          'post_type' => 'event',
+          'meta_key' => 'event_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            ),
+            array(
+              'key' => 'related_programs',
+              'compare' => 'LIKE',
+              'value' => '"' . get_the_ID() . '"'
+            )
+          )
+        ));
 
+        if ($homepageEvents->have_posts()) {
+          echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
 
-    <?php
+        while($homepageEvents->have_posts()) {
+          $homepageEvents->the_post();
+          get_template_part('template-parts/content-event');
+        }
+        }
 
-    wp_reset_query(  );
-            $today = date('Ymd');
+        wp_reset_postdata();
+        $relatedCampuses = get_field('related_campus');
 
-        $upcoming_events = new WP_Query([
-            'posts_per_page' => -1,
-            'post_type' => 'event',
-            'meta_key' => 'event_date',
-            'orderby' => 'meta_value_num',
-            'order' => 'ASC',
-            'meta_query' => [
-                [
-                    'key' => 'event_date',
-                    'compare' => '>=',
-                    'value' => $today,
-                    'type' => 'numeric'
-                ],
-                [
-                    'key' => 'related_programs',
-                    'compare' => 'LIKE',
-                    'value' => '"' . get_the_ID() . '"',
-                ]
-            ]
-        ]);
+        if ($relatedCampuses) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">' . get_the_title() . ' is Available At These Campuses:</h2>';
 
-    if($upcoming_events->have_posts()) {
-        echo '<hr class="section-break">';
+          echo '<ul class="min-list link-list">';
+          foreach($relatedCampuses as $campus) {
+            ?> <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus) ?></a></li> <?php
+          }
+          echo '</ul>';
 
-        echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' events</h2>';
-       
-        while($upcoming_events->have_posts()) {
-            $upcoming_events->the_post();
-            ?>
+        }
 
-    <div class="event-summary">
-        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-            <span class="event-summary__month"><?php the_time('M'); ?></span>
-            <span class="event-summary__day"><?php the_time('d'); ?></span>
-        </a>
-        <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a
-                    href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18) ; ?> <a href="<?php the_permalink(); ?>"
-                    class="nu gray">Learn more</a>
-            </p>
-        </div>
+      ?>
+
     </div>
-    <?php
-        }
+    
 
-    }
+    
+  <?php }
 
-    ?>
-
-
-</div>
-
-
-
-<?php }
-
-get_footer();
+  get_footer();
 
 ?>
